cc5dc20213fcc01683bba999a2abc79bea9756cf
JackSteffan <150395486+JackSteffan@users.noreply.github.com>
2023-12-04 21:48:08 -0600

    Restarted Project due to merge conflict.

---
+++ Models/Song.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_5.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Performer { get; set; }
+        
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
