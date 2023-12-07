482f752f734767671c7f0b5cad2804b7a3532057
Clovo <72047946+Clovotc@users.noreply.github.com>
2023-12-06 21:17:39 -0600

    Created the Genre Search Bar

---
+++ Models/SongGenreView.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Assignment_5.Models
+{
+    public class SongGenreView
+    {
+        public List<Song>? Songs { get; set; }
+        public SelectList? Genres { get; set; }
+        public string? SongGenre { get; set; }
+        public string? SearchString { get; set; }
+    }
+}
